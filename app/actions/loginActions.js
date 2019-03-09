60c8d0fd559df483c4296ae9454ab44007fd742d
Hoang Quoc Huynh <hoang_quoc_huynh@i-hayabusa.com>
2019-03-09 20:10:04 +0700

    ADd action

--- app/actions/loginActions.js
+++ app/actions/loginActions.js
@@ -1,5 +1,19 @@
-import { INIT_LOGIN, LOGIN_SUCCESS, LOGIN_FAIL } from "./types";
+import { INIT_LOGIN, LOGIN_SUCCESS, LOGIN_FAIL, LOGIN_LOADING } from "./types";
 
 export const initLogin = () => {
   return { type: INIT_LOGIN };
+};
+
+const loginFail = (dispatch, error) => {
+  dispatch({
+    type: LOGIN_FAIL,
+    payload: error
+  });
+};
+
+const loginSuccess = (dispatch, user) => {
+  dispatch({
+    type: LOGIN_SUCCESS,
+    payload: user
+  });
 };
--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from "redux";
+import login from "./loginReducer";
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  login
+});
 
 export default rootReducer;
